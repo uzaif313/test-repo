4532c1be4db72b9c9ac0f07358ec4d295236e003
uzaif nilger <uzaif313@gmail.com>
2016-12-23 21:45:44 +0530

    #2 Fixed continues loading issue

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ var http = require("http");
 
 http.createServer(function(request,response){
     console.log("It's Time to rock and roll with node");
+    res.end("Just stop loading now");
 }).listen(3000);
 
